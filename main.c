0381909ac484ac39537ce4d818ff40f84e6ebabd
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2023-03-16 22:11:02 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 *****************************************************/
 
+#include <stdio.h>
 #include "node.h"
 #include "queue.h"
 #include "stack.h"
@@ -9,7 +10,9 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 
 int main() {
 	
-	// Your code here
+	printf("helo");
+	displayQueue(createQueue(10));
+	printf("helo");
 	
 	return 0;
 }
