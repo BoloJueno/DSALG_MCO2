f1ca67a80773f9b67958fdc48bea65a7d53bab33
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2023-03-16 23:14:22 +0800

    Update main.c

--- main.c
+++ main.c
@@ -10,16 +10,26 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 // #include "algorithms.h"
 
 int main() {
+
+	char string[10] = "hi";
 	
-	queue q = *(createQueue(10));
-	queue* qPtr = &q;
-	queue** ptrPtr = &qPtr;
+	// queue q = *(createQueue(10));
+	// queue* qPtr = &q;
+	// queue** qPtrPtr = &qPtr;
+
+	stack s = *(createStack(10));
+	stack* sPtr = &s;
+	stack** sPtrPtr = &sPtr;
 
 	printf("helo\n");
-	printf("%d", queueFull(&q));
-	enqueue(ptrPtr, "1");
+	// printf("%d", queueFull(&q));
+	// enqueue(qPtrPtr, "1");
+	// printf("helo\n");
+	// printf("%d", queueEmpty(&q));
+
+	printf("%d", stackFull(sPtr));
+	push(sPtrPtr, string);
 	printf("helo\n");
-	printf("%d", queueEmpty(&q));
 	
 	return 0;
 }
