858aee40c2c017ea8205c094e33e196f3d04c89c
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2023-03-16 22:56:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 *****************************************************/
 
 #include <stdio.h>
+#include <string.h>
 #include "node.h"
 #include "queue.h"
 #include "stack.h"
@@ -11,7 +12,12 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 int main() {
 	
 	queue q = *(createQueue(10));
+	queue* qPtr = &q;
+	queue** ptrPtr = &qPtr;
 
+	printf("helo\n");
+	printf("%d", queueFull(&q));
+	enqueue(ptrPtr, "1");
 	printf("helo\n");
 	printf("%d", queueEmpty(&q));
 	
