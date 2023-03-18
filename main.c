edb382305be9a6108c9dc6a96b1a509100a3a8c4
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2023-03-18 19:42:45 +0800

    included algo.h

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 #include "node.h"
 #include "queue.h"
 #include "stack.h"
-// #include "algorithms.h"
+#include "algorithms.h"
 
 int main() {
 
